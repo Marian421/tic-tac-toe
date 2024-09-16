393c2a5a5b7a353ce6567a4587e88a0982dfafaf
Herciu Marian <herciumarian8@gmail.com>
2024-09-16 15:24:32 +0300

    Make display work

--- script.js
+++ script.js
@@ -99,21 +99,61 @@ const game = (function GameController(
         return `${getActivePlayer().name}'s turn`;
     }
 
+    function checkForWinOrDraw(row, column) {
+        const boardWithMarks = Gameboard.getBoardWithMarks();
+        let mark = getActivePlayer().token;
+        
+        // check current row
+        if (boardWithMarks[row][0] === mark && boardWithMarks[row][1] === mark && boardWithMarks[row][2] === mark)
+        {
+            return "win";
+        }
+
+        // check current column 
+        if (boardWithMarks[0][column] === mark && boardWithMarks[1][column] === mark && boardWithMarks[2][column] === mark)
+        {
+            return "win";
+        }
+
+        // check main diagonal
+        if (boardWithMarks[0][0] === mark && boardWithMarks[1][1] === mark && boardWithMarks[2][2] === mark) {
+            return "win";
+        }
+
+        // check second diagonal
+        if(boardWithMarks[2][0] === mark && boardWithMarks[1][1] === mark && boardWithMarks[0][2] === mark) {
+            return "win";
+        }
+
+        // check to see if there are spots left on the board    
+        function checkFor0(){
+            for (let i = 0; i < 3; i++){
+                for (let j = 0; j < 3; j++){
+                   if (boardWithMarks[i][j] == 0){
+                    return true;
+                   }
+                }
+            }
+        }
+        if (!checkFor0())
+        {
+            return "draw";
+        }
+    }
+
     const playRound = (row, column) => {
         console.log(`Putting ${getActivePlayer().name}'s mark on spot ${row} ${column}`);
         if (Gameboard.mark(getActivePlayer().token, row, column) === 'false'){
             console.log("Failed");
             return
         }
-
-        const boardWithMarks = Gameboard.getBoardWithMarks();
-        let win = 0;
-        let spaceForMarks = 1;
-        for (let i = 0; i < 2; i++)
+        let condition = checkForWinOrDraw(row,column);
+        if (condition === "win")
         {
-            
+            console.log("win");
+        } else if (condition === "draw"){
+            console.log("draw");
         }
-
         switchPlayerTurn();
         printNewRound();
     }
@@ -126,7 +166,41 @@ const game = (function GameController(
 })();
 
 const ScreenControler = (function(){
-    const newRound = document.querySelector('.newRound');
-    newRound.innerHTML = game.printNewRound();
+    const playerTurn = document.querySelector('.newRound');
+    const boardDiv = document.querySelector('.board');
 
+    const updateScreen = () => {
+        // clear board
+        boardDiv.textContent = "";
+        console.log("I'm getting called");
+        // get board and active player
+        const board = Gameboard.getBoard();
+        const activePlayer = game.getActivePlayer();
+
+        // update whichs player turn it is
+        playerTurn.textContent = `${activePlayer.name}'s turn...`;
+
+        for (let i = 0; i < 3; i++){
+            for (let j = 0 ; j < 3 ; j++){
+                // create a cell
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+        
+                // put the mark on the cell
+                if (board[i][j].getMark() !== 0){
+                cellButton.textContent = board[i][j].getMark();
+            } else {
+                cellButton.textContent = "";
+            }
+                cellButton.addEventListener("click", () => {
+                    game.playRound(i,j);
+                    updateScreen();
+            })
+                boardDiv.appendChild(cellButton);
+        }
+
+    }
+
+}
+    updateScreen(); 
 })();
